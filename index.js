0a67f405c3c4ca96029205853f015691ff2ef8fc
Dexter Ramos <blitzdex27@gmail.com>
2021-04-01 16:53:31 +0800

    Converted HTML files ito EJS, and added get requests.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 const express = require("express");
+const bodyParser = require("body-parser")
+
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + "/public"))
+
+app.route("/")
+    .get((req, res) => {
+        res.render("home")
+    })
+
+app.route("/user-blogs")
+    .get((req, res) => {
+        res.render("userBlogs")
+    })
+
+app.route("/about")
+    .get((req, res) => {
+        res.render("about")
+    })
+
+
+app.route("/blog")
+    .get((req, res) => {
+        res.render("blog")
+    })
+
+
+
+app.route("/signin")
+    .get((req, res) => {
+        res.render("signin")
+    })
+
+app.route("/signup")
+    .get((req, res) => {
+        res.render("signup")
+    })
+
+
+// Set the app to listen at port 3000
+let port = process.env.PORT 
+if (port == null || port == ""){
+    port = 3000
+}
+app.listen(port, () => {
+    console.log("Server has started at port " + port)
+})
