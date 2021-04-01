dcd5739bda109cb77b930316ed8d1620a7d7b71a
Dexter Ramos <blitzdex27@gmail.com>
2021-04-01 17:03:49 +0800

    Refactor page routes

---
+++ components/routes/pageRoutes.js
@@ -0,0 +1,38 @@
+
+
+module.exports = (app) => {
+
+app.route("/")
+.get((req, res) => {
+    res.render("home")
+})
+
+app.route("/user-blogs")
+.get((req, res) => {
+    res.render("userBlogs")
+})
+
+app.route("/about")
+.get((req, res) => {
+    res.render("about")
+})
+
+
+app.route("/blog")
+.get((req, res) => {
+    res.render("blog")
+})
+
+
+
+app.route("/signin")
+.get((req, res) => {
+    res.render("signin")
+})
+
+app.route("/signup")
+.get((req, res) => {
+    res.render("signup")
+})
+
+}
--- index.js
+++ index.js
@@ -8,38 +8,10 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/public"))
 
-app.route("/")
-    .get((req, res) => {
-        res.render("home")
-    })
-
-app.route("/user-blogs")
-    .get((req, res) => {
-        res.render("userBlogs")
-    })
-
-app.route("/about")
-    .get((req, res) => {
-        res.render("about")
-    })
 
 
-app.route("/blog")
-    .get((req, res) => {
-        res.render("blog")
-    })
-
-
-
-app.route("/signin")
-    .get((req, res) => {
-        res.render("signin")
-    })
-
-app.route("/signup")
-    .get((req, res) => {
-        res.render("signup")
-    })
+// Page Routes
+require("./components/routes/pageRoutes")(app)
 
 
 // Set the app to listen at port 3000
